d6586babedaa45c5c0368ceb34ee950876140bda
noyechan <noy3928@naver.com>
2023-08-12 19:24:12 +0900

    feat : implement api

---
+++ src/main/java/com/example/blogProject/controller/BlogApiController.java
@@ -0,0 +1,28 @@
+package com.example.blogProject.controller;
+
+import com.example.blogProject.domain.Article;
+import com.example.blogProject.dto.AddArticleRequest;
+import com.example.blogProject.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    // HTTP 메서드가 POST일 때 전달받은 URL과 동일하면 메서드로 매핑
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+}
