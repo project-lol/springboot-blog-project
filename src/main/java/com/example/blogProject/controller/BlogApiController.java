bd5443724e909133a1ef344b60af43cf7ef3dace
noyechan <noy3928@naver.com>
2023-08-12 19:58:58 +0900

    feat : make api for find one article by id

--- src/main/java/com/example/blogProject/controller/BlogApiController.java
+++ src/main/java/com/example/blogProject/controller/BlogApiController.java
@@ -36,4 +36,12 @@ public class BlogApiController {
         return ResponseEntity.ok()
                 .body(articles);
     }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
 }
